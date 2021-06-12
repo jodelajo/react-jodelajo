104c1526cd1e6ed67fbd3b4d7dd3ef7146efbe3c
jodelajo <jodelajo@gmail.com>
2021-06-12 16:29:26 +0200

    about

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -7,18 +7,16 @@ function About() {
       <h1>Aangenaam,</h1>
 
       <p>
-        Mijn naam is Joanneke Haasbroek en ik ben Frontend Developer. 
-      </p>
-      <p>
+        Mijn naam is Joanneke Haasbroek en ik ben <span className={styles.accent}>Frontend Developer</span>.
         In 2019 heb ik het roer omgegooid en de sprong in het diepe gewaagd en een carriere-switch gemaakt. </p>
-        <p>Na jaren te hebben gewerkt als zelfstandig projectmedewerker bij een maatschappelijke stichting, was ik niet meer gelukkig in mijn werk. 
-      </p>
-      
-   
+        <p>Na jaren te hebben gewerkt als zelfstandig projectmedewerker bij een maatschappelijke stichting, was ik niet meer gelukkig in mijn werk. Ik had al een opleiding Webdesign afgerond in 2009 waar ik veel plezier aan had beleefd. Al ruim 20 jaar werk ik met Photoshop. </p>
+        <h3>Design <span className={styles.red}>&</span>& Coding</h3>
+        <p>Het afgelopen jaar heb ik mij volledig toegelegd op het ontwikkelen van skills en kennis op het gebied van Frontend Development en vormgeving. Ik heb voor voor <span className={styles.accent}>Adobe Photoshop</span>, <span className={styles.accent}>Illustrator</span> en <span className={styles.accent}>InDesign</span> mijn <span className={styles.accent}>MBO-certificaten</span> behaald en momenteel wacht ik op de uitslag van mijn eindopdracht van de opleiding <span className={styles.accent}>Frontend Development</span> aan de Novi Hogeschool in Utrecht.</p>
 
-      <p>
-          Geen HBO-diploma, nee, nein, njet. Wel 10 jaar ervaring op functie met HBO-profiel.
-      </p>
+        <p>Ik vind het snijvlak van design en coderen het allerleukst. Pixelperfect en gebruikersvriendelijk met een karaktervolle uitstraling.</p>
+
+        <p>Spreekt dit je aan? Dan kun je altijd contact opnemen voor een nadere kennismaking.</p>
+   
     </div>
   );
 }
