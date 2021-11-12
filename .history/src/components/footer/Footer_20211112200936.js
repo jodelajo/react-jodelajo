3c195e253be328cad03ab47c583fe1d20d8e9918
jodelajo <jodelajo@gmail.com>
2021-11-12 23:22:47 +0100

    nice button-size

---
+++ .history/src/components/footer/Footer_20211112200936.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import { IoLogoGithub, IoLogoLinkedin, IoMail } from "react-icons/io5";
+import ThemeButton from "../themeButton/ThemeButton";
+import Neon from "../neon/Neon";
+
+function Footer({ themeHandler }) {
+  const day = new Date();
+  const year = day.getFullYear();
+
+  return (
+    <div className={styles["footer-container"]}>
+      <div className={styles.buttonContainer}>
+        <ThemeButton onClick={themeHandler} />
+      </div>
+      <div className={styles.neon}>
+        <Neon />
+      </div>
+      <div className={styles["info-container"]}>
+        <p className={styles["contact-text"]}>
+          GirlPowered by: jodelajo {year}
+        </p>
+        <div className={styles.icons}>
+          <span className={styles["contact-icons"]}>
+            <a href="mail.to:jodelajo@gmail.com">
+              <IoMail className={styles.mail} />
+            </a>
+          </span>
+          <span className={styles["contact-icons"]}>
+            <a
+              href="https://www.linkedin.com/in/joanneke-haasbroek-77b31020/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <IoLogoLinkedin />
+            </a>
+          </span>
+          <span className={styles["contact-icons"]}>
+            <a
+              href="https://github.com/jodelajo"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <IoLogoGithub />
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Footer;
